acf9ba744cfa8d64ae33f6f02d433564bd59741a
salmaCal <salmabalmir55@gmail.com>
2026-04-08 19:03:48 +0100

    commit

--- src/main/java/net/balmir/sdiaspringmvc/web/ProductController.java
+++ src/main/java/net/balmir/sdiaspringmvc/web/ProductController.java
@@ -1,11 +1,53 @@
 package net.balmir.sdiaspringmvc.web;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import net.balmir.sdiaspringmvc.entities.Product;
 import net.balmir.sdiaspringmvc.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 @Controller
 public class ProductController {
-        @Autowired
-        private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @GetMapping("/user/index")
+    @PreAuthorize("hasRole('USER')")
+    public String index(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productList", products);
+        return "products";
     }
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/user/index";
+    }
+    @GetMapping("/delete)")
+    public String delete(@RequestParam(name = "id") Long id){
+        productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product , BindingResult bindingResult , Model model) {
+        if (bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return "redirect:/newProduct";
+    }
+
+}
 
 
